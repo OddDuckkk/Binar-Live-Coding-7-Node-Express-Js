73acadd020f8ba89077a00f3ab99c08d7b1046b3
Brandon OddDuckk <Brandon.Ogaden@gmail.com>
2024-10-04 20:06:46 +0800

    feat: Added get car by id api

--- server.js
+++ server.js
@@ -26,6 +26,7 @@ app.get('/brandon', (req, res) => {
 })
 
 //  /api/v1/collections 
+// Api get all cars
 app.get('/api/v1/cars', (req, res) => {
     res.status(200).json({ 
         status: "Success",
@@ -38,6 +39,7 @@ app.get('/api/v1/cars', (req, res) => {
     });
 })
 
+// Api create/insert cars
 app.post('/api/v1/cars', (req, res) => {
     // insert into
 
@@ -54,6 +56,33 @@ app.post('/api/v1/cars', (req, res) => {
                 car: newCar,
             }
         });
+    });
+})
+
+// api to get cars by id
+app.get('/api/v1/cars/:id', (req, res) => {
+    // select * from cars where id="1" OR NAME="jazz"
+    // console.log(req.params);
+    const carId = req.params.id;
+    const car = cars.find((i) => i.id ==  carId);
+    
+    // error handling
+    if (!car) {
+        return res.status(404).json({
+            status: "Failed",
+            message: `Car with id ${carId} not found`,
+            isSuccess: false,
+            data: null,
+        });  // 404 Not Found  // 200 OK  // 500 Internal Server Error  // 400 Bad Request  // 304 Not Modified  // 201 Created  // 403 Forbidden  // 302 Found  // 401 Unauthorized  // 307 Temporary Redirect  // 308 Permanent Redirect  // 202 Accepted  // 206 Partial Content  // 412 Precondition Failed  // 417 Expectation Failed  // 422 Unprocessable Entity  // 429 Too Many Requests  // 431 Request Header Fields Too Large  // 451 Unavailable For Legal Reasons  // 502 Bad Gateway  //
+
+    }
+    res.status(200).json({ 
+        status: "Success",
+        message: "Success get car data by id",
+        isSuccess: true, 
+        data: {
+            car,
+        },
     });
 
     
